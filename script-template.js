9d06f14f9547d59c43d8cfcf1ed98c92896b0b48
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-09-25 13:35:59 -0400

    added demo message

---
+++ script-template.js
@@ -0,0 +1,22 @@
+let buffer; // store user input
+
+// handle buffer submit
+terminalForm.addEventListener("submit", e => {
+    e.preventDefault();
+    buffer = terminalInput.value;
+    bhManage(); // manage buffer history
+
+
+	/*============================================*/
+	/* Replace this block with your submit actions */
+
+    if(buffer == "clear")
+        cls()
+    else
+        printf(buffer); // echo input
+
+	/*                                            */
+	/*============================================*/
+
+    terminalInput.value = "";
+});
--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const userPrompt = 'Type anything for ';
+
+const msg = [
+	'This is an interactive web terminal. Type anything or press enter for more info.',
+	'Type "clear" to clear screen. The up and down arrow keys will cycle through your history.',
+	'You can edit how the terminal handles input in "script.js" (You can copy and rename "script-template.js" for a blank template).',
+	'Source code at: <a href="https://github.com/mattConn/interactive-web-terminal">github.com/mattConn/interactive-web-terminal</a>'
+];
+
 let buffer; // store user input
+
+// initial msg
+printf('$ '+msg.shift());
 
 // handle buffer submit
 terminalForm.addEventListener("submit", e => {
@@ -6,17 +18,18 @@ terminalForm.addEventListener("submit", e => {
     buffer = terminalInput.value;
     bhManage(); // manage buffer history
 
+     
+	if(buffer.length) printf('> '+buffer); // echo input
 
-	/*============================================*/
-	/* Replace this block with your submit actions */
+	switch(buffer)
+	{
+		case 'clear':
+			cls();
+			break;
 
-    if(buffer == "clear")
-        cls()
-    else
-        printf(buffer); // echo input
-
-	/*                                            */
-	/*============================================*/
+		default:
+			if(msg.length) printf('$ '+msg.shift());
+	}
 
     terminalInput.value = "";
 });
